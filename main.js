aaf5d14b99e86a18a7d6f90acd606b765493bb4e
Darakhshan Juniad <156326631+Darakhshan2@users.noreply.github.com>
2024-04-30 00:00:24 -0700

    Add files via upload
    This is the class_08

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// ARRAY : "Storing value of multiple data types in a single variable"
+// syntex : "pre defined method to call"
+let car = ["BMW", "FERRARI", "COROLA", "ALTO", "LAMBORGINI"];
+// variable => array => elemennts => elements type => string
+//console.log(car[0]);
+let count = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13];
+//        indices =[0,1,2,3,4,5,6,7,8,9, 10,11,12]
+//console.log(count[12]);
+//console.log(count.length);// length = no of elements store                                                  
+//  static : unupdateable
+// dynamic : updateable 
+let friends = ["minal", "konain", "maryam", "laiba"];
+console.log(friends);
+friends.push("alyana");
+console.log(friends);
+friends.pop();
+console.log(friends);
---
+++ main.ts
@@ -0,0 +1,18 @@
+// ARRAY : "Storing value of multiple data types in a single variable"
+// syntex : "pre defined method to call"
+let car:string[]=["BMW","FERRARI","COROLA","ALTO","LAMBORGINI"];
+// variable => array => elemennts => elements type => string
+//console.log(car[0]);
+let count:number[]=[1,2,3,4,5,6,7,8,9,10,11,12,13];
+//        indices =[0,1,2,3,4,5,6,7,8,9, 10,11,12]
+//console.log(count[12]);
+//console.log(count.length);// length = no of elements store                                                  
+//  static : unupdateable
+// dynamic : updateable 
+let friends = ["minal","konain","maryam","laiba"];
+console.log(friends);
+friends.push("alyana");
+console.log(friends);
+friends.pop();
+console.log(friends);
+
